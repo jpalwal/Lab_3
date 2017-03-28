2b03f7531069420873b87ce5ee2a15ff36858c3d
jpalwal <jagoda.palwal@gmail.com>
2017-03-28 12:39:43 +0200

    create validator

--- validator.py
+++ validator.py
@@ -1,1 +1,25 @@
+import abc
 
+class NotANumber(Exception):
+    pass
+
+
+class AbstractValidator(object):
+    __metaclass__  = abc.ABCMeta
+    @abc.abstractmethod
+    def validate(self, to_be_validated):
+        """validate the input"""
+
+
+class InputExpressionValidator(AbstractValidator):
+
+    def __init__(self, to_be_validated):
+        self.expression = to_be_validated
+
+    def validate(self):
+        if not self._is_number():
+            raise NotANumber()
+        else:
+            return True
+    def _is_number(self):
+        return isinstance(self.expression, int)
