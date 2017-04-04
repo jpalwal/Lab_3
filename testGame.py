1485e8de8cab599a2d18069977331a73e07a9926
jpalwal <jagoda.palwal@gmail.com>
2017-04-04 20:00:08 +0200

    add test for overwriting coords

--- testGame.py
+++ testGame.py
@@ -12,6 +12,14 @@ class TestGame(unittest.TestCase):
         tic_tac_toe.choose_square(0,3)
         self.assertEqual(game.CheckGameResult(),tic_tac_toe.game_over())
 
+    def test_shoud_not_overrite_coords_if_ocupated(self):
+        board_size = 4
+        tic_tac_toe = game.Game(board_size)
+        tic_tac_toe.choose_square(1,2)
+        tic_tac_toe.switch_player()
+        tic_tac_toe.choose_square(1,2)
+        self.assertEqual(tic_tac_toe[1,2],game.MarkSquare.CROSS)
+
 
 if __name__=='__main__':
     unittest.main()
