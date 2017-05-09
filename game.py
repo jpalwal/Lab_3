953ff5b6e8882552f281c340a1298f1645ed867e
jpalwal <jagoda.palwal@gmail.com>
2017-05-09 16:08:39 +0200

    change output and checjing if game is over, add logging

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import validator
 import enum
-
+import logging
+from os import path, remove
 
 class Game:
     def __init__(self, board_size):
@@ -8,41 +9,87 @@ class Game:
         self.emptySquares = board_size**2
         self.board = [[MarkSquare.FREE for x in range(board_size)] for y in range(board_size)]
         self.nowPlaying = 'u'
+        self.boardSize = board_size
+        if path.isfile("game.log"):
+            remove("game.log")
+        logging.basicConfig(filename="game.log", level=logging.INFO)
+        logging.info("Game initialization")
+
 
     def choose_square(self,x,y):
-        if self.checkMove.validate(x,y, len(self.board)):
-            if self.board[y][x] == MarkSquare.FREE:
-                self.board[y][x] = MarkSquare.CROSS if self.nowPlaying == 'u' else MarkSquare.CIRCLE
-                self.emptySquares -= 1
-                return True
-            else:
-                return False
+        logging.info("Check if coords are ok and if field ({}, {}) is emptyd to place a marker".format(x, y))
+        try:
+            self.checkMove.validate(x,y, len(self.board))
+        except validator.OutOfBoard():
+            logging.info("Cordinates ({}, {}) are invalid".format(x, y))
+            return -1
+        if self.board[y][x] == MarkSquare.FREE:
+            self.board[y][x] = MarkSquare.CROSS if self.nowPlaying == 'u' else MarkSquare.CIRCLE
+            self.emptySquares -= 1
+            logging.info("field ({}, {}) is empty and marker placed".format(x, y))
+            return True
+        else:
+            logging.info("field ({}, {}) is not empty and quit".format(x, y))
+            return False
+        #else:
+         #   logging.info("wrong coordinates ({}, {})".format(x, y))
+          #  print("zle wspolrzedne")
 
     def switch_player(self):
         self.nowPlaying = 'u' if self.nowPlaying == 'c' else 'c'
+        logging.info("switching player for user") if self.nowPlaying == 'c' else logging.info("switching player for computer")
 
     def game_over(self):
         if not self.emptySquares:
+            logging.info("No empty squares, draw")
             return CheckGameResult.DRAW
+        check = 0
+        #horizontal
         for i in range(len(self.board)):
             for j in range((len(self.board))):
-                if self.board[i][j] == 'o':
-                    break
-            else:
+                if self.board[i][j] == MarkSquare.CIRCLE or self.board[i][j] == MarkSquare.FREE:
+                    check=1
+            if not check:
+                logging.info("horizontal winning, winner is cross")
                 return CheckGameResult.CROSS
+            check = 0
         for i in range(len(self.board)):
             for j in range((len(self.board))):
-                if self.board[i][j] == 'x':
-                    break
-            else:
+                if self.board[i][j] == MarkSquare.CROSS or self.board[i][j] == MarkSquare.FREE:
+                    check = 1
+            if not check:
+                logging.info("horizontal winning, winner is circle")
+                return CheckGameResult.CIRCLE
+            check = 0
+        #vertical
+        for i in range(len(self.board)):
+            for j in range((len(self.board))):
+                if self.board[j][i] == MarkSquare.CIRCLE or self.board[j][i] == MarkSquare.FREE:
+                    check=1
+            if not check:
+                logging.info("vertical winning, winner is cross")
                 return CheckGameResult.CROSS
+            check = 0
+        for i in range(len(self.board)):
+            for j in range((len(self.board))):
+                if self.board[j][i] == MarkSquare.CROSS or self.board[j][i] == MarkSquare.FREE:
+                    check = 1
+            if not check:
+                logging.info("vertical winning, winner is circle")
+                return CheckGameResult.CIRCLE
+            check = 0
+        logging.info("no winner, still playing")
         return CheckGameResult.PLAYING
 
     def computer_turn(self):
+        logging.info("checking whose turn it is")
+        print("check if comp turn")
+        print(self.nowPlaying == 'c')
         return self.nowPlaying == 'c'
 
     def board(self):
         return self.board
+
 
 
 class MarkSquare(enum.Enum):
@@ -52,41 +99,42 @@ class MarkSquare(enum.Enum):
 
 
 class CheckGameResult(enum.Enum):
-    PLAYING = 0
+    PLAYING = 'p'
     CIRCLE = 'o'
     CROSS = 'x'
     DRAW = 'r'
 
 
-# zapis jest źle
+
 class MakeAndPrintBoard():
     @staticmethod
     def print_board(board):
+        print("print_board")
         print('\n')
-        print('\t')
-        print(''.join(str([i for i in range(len(board.board))])))
-        for i in range(len(board.board)):
-            marker = '{}\t'.format(i)
-            for j in range(len(board.board[i])):
+        print('\t' + ''.join(['{}    '.format(i) for i in range(0, len(board.board))]))
+        for i in range(0, len(board.board)):
+            marker=[]
+            for j in range(0, len(board.board[i])):
                 if board.board[i][j] == MarkSquare.FREE:
-                    marker.join('_')
+                    marker.append('_')
                 elif board.board[i][j] == MarkSquare.CROSS:
-                    marker.join('X')
+                    marker.append('X')
                 elif board.board[i][j] == MarkSquare.CIRCLE:
-                    marker.join('O')
-            marker.join('\t')
-            marker.join(str(i))
-            print(marker)
-        print('\t')
-        print(''.join(str([i for i in range(len(board.board))])))
+                    marker.append('O')
+                else:
+                    marker.append('_')
+            print(i, marker)
+        print('\t' + ''.join(['{}    '.format(i) for i in range(0, len(board.board))]))
 
 
 class Lan:
     @staticmethod
     def send_data(connection, data):
+        print("send_data")
         connection.sendall('#start#{}#end'.format(data).encode('ASCII'))
     @staticmethod
     def request_data(connection):
+        print("request")
         data=''
         while True:
             buffer=connection.recv(16)
@@ -102,5 +150,6 @@ class Lan:
         return data.replace('#start#','').replace('#end','')
     @staticmethod
     def send_and_request_data(connection, data):
+        print("send and request")
         Lan.send_data(connection,data)
         return Lan.request_data(connection)
