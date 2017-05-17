38e8ab16ecd505363a6caf781ba8e8d3a14bb8a6
jpalwal <jagoda.palwal@gmail.com>
2017-05-17 11:30:55 +0200

    add logging

--- serwer/echoClient.py
+++ serwer/echoClient.py
@@ -1,26 +1,35 @@
 import socket
 import sys
+import logging
+from os import path, remove
 
 
 class MyEchoClient:
     def __init__(self, address, port, data_size):
         self.data_size = data_size
-        self.createTcpIpSocket()
-        self.connectToServer(address, port)
+        self._createTcpIpSocket()
+        self._connectToServer(address, port)
+        if path.isfile("client.log"):
+            remove("client.log")
+        logging.basicConfig(filename="client.log", level=logging.INFO)
+        logging.info("MyEchoClient initialization")
 
     def sendMsg(self, msg):
         self.sock.send(msg)
         response = self.sock.recv(self.data_size)
         self.sock.close()
         print('recieve %s ' % response)
+        logging.info("Recieve %s " % response)
 
-    def createTcpIpSocket(self):
+    def _createTcpIpSocket(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        logging.info("Create socket")
 
-    def connectToServer(self, address, port):
+    def _connectToServer(self, address, port):
         server_address = (address, port)
         print('connecting to %s port %s' % server_address)
         self.sock.connect(server_address)
+        logging.info("Connect to %s port %s" % server_address)
 
 
 if __name__=='__main__':
@@ -29,4 +38,3 @@ if __name__=='__main__':
     port = 50001
     data_size = 1024
     client = MyEchoClient(host, port, data_size)
-    client.sendMsg("Hello")
