e4fc6d420f1c918f0ed4daeae721e479c63136a3
jpalwal <jagoda.palwal@gmail.com>
2017-05-17 11:23:06 +0200

    add logging

--- serwer/echoServer.py
+++ serwer/echoServer.py
@@ -1,34 +1,39 @@
 import socket
 import sys
+import logging
+from os import path, remove
 
 class MyEchoServer:
     def __init__(self, address, port, data_size):
         self.data_size = data_size
         self.createTcpIpSocket()
         self.bindSocketToThePort(address, port)
+        if path.isfile("server.log"):
+            remove("server.log")
+        logging.basicConfig(filename="server.log", level=logging.INFO)
+        logging.info("MyEchoServer initialization")
 
     def handle_connection(self):
-        print('handle')
+        logging.info("Handling connection")
         self.sock.listen(1)
-        while True:
-            connection, client_address = self.sock.accept()
-            data = connection.recv(self.data_size)
-            print('waiting for connection')
-            if data:
-                print(data)
-                connection.send(data)
-            connection.close()
+        #while True:
+        logging.info("Waiting for connection")
+        connection, client_address = self.sock.accept()
+        data = connection.recv(self.data_size)
+        if data:
+            logging.info("Data recevied")
+            connection.send(data)
+        connection.close()
 
     def createTcpIpSocket(self):
-        print('create')
+        logging.info("Socket created")
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def bindSocketToThePort(self, address, port):
-        print('bind')
         server_address = (address, port)
         print('bind to %s port %s' % server_address)
         self.sock.bind(server_address)
-        print('bind2')
+        logging.info("Bind to %s port %s" % server_address)
 
 if __name__=='__main__':
 
